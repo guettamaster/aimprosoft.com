c6fd8e290852beb526077af70b6e4fc04c018751
user-qa <guettad@mail.ru>
2017-08-31 17:55:20 +0300

    test3108

---
+++ src/test/java/aimprosoft/com/DefinitionTestSuite.java
@@ -0,0 +1,9 @@
+package aimprosoft.com;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features="src/test/resources/HomePage.feature")
+public class DefinitionTestSuite {}
---
+++ src/test/java/aimprosoft/com/LOCATORS.java
@@ -0,0 +1,18 @@
+package aimprosoft.com;
+
+public interface LOCATORS {
+
+//TODO======================================================HomePage========================================================
+
+String AIMPROSOFT_LOGO = "//div[@class='navbar-header']/a[@class='navbar-brand']";
+String LEFT_BUTTON = "//div[@class='menu-icon']";
+String AIMPROSOFT_LOGO_ON_THE_FULL_SIZE_SITE_MODE = "//div[@class='site-logo']/a[@class='navbar-brand']";
+String CATEGORY_TITLE_NAME_ON_THE_NAVIGATION_MENU = "//a[contains(text(),'$1')]";
+String SUBCATEGORY_TITLE_NAME_ON_THE_NAVIGATION_MENU = "//a[contains(text(),'$1')]";
+
+
+
+
+
+
+}
