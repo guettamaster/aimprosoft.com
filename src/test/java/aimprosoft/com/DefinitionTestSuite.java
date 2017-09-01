bcf1d720b4e7d4a828561cb7ffa007f279b36ed0
user-qa <guettad@mail.ru>
2017-09-01 15:36:32 +0300

    update 01.09.17

--- src/test/java/aimprosoft/com/DefinitionTestSuite.java
+++ src/test/java/aimprosoft/com/DefinitionTestSuite.java
@@ -5,5 +5,7 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/HomePage.feature")
+@CucumberOptions(features= {"src/test/resources/HomePage.feature",
+                            "src/test/resources/NavigationMenu.feature"
+})
 public class DefinitionTestSuite {}
